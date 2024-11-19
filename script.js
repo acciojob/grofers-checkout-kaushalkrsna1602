5d0e4bbda1936a91c53987f7841cf36fd64ae887
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-11-19 22:34:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,21 +4,12 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
     const prices = document.querySelectorAll(".price");
-    let total = 0;
-    prices.forEach(price => {
-        total += parseInt(price.textContent);
+    let totalPrice = 0;
+    prices.forEach((price) => {
+        totalPrice += parseFloat(price.textContent.trim());
     });
-    
-    const table = document.querySelector("table");
-    const totalRow = document.createElement("tr");
-    
-    const totalCell = document.createElement("td");
-    totalCell.setAttribute("colspan", 2);
-    totalCell.textContent = `Total Price: Rs ${total}`;
-    totalCell.style.fontWeight = "bold";
-    
-    totalRow.appendChild(totalCell);
-    table.appendChild(totalRow);
+    const totalDiv = document.getElementById("ans");
+    totalDiv.textContent = `Total: Rs ${totalPrice}`;
 };
 
 getSumBtn.addEventListener("click", getSum);
